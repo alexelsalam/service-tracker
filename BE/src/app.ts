e6576bafca4ec2df56e4914b209bfaa2a86a26ee
alexelsalam <alexelsalam.el@gmail.com>
2026-05-20 13:18:41 +0700

    change server

--- BE/src/app.ts
+++ BE/src/app.ts
@@ -52,9 +52,8 @@ app.use(errorHandler);
 // =====================
 // Start Server
 // =====================
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("API berjalan");
 });
 
 export default app;
